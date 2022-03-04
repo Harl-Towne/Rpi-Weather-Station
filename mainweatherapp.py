002d90f68064372fef428cd4658e72d32daf3e46
Harl Towne <harl.towne@connect.qut.edu.au>
2022-03-04 10:09:17 +1000

    Sync

--- mainweatherapp.py
+++ mainweatherapp.py
@@ -57,10 +57,10 @@ class mainWeatherWindow(QMainWindow, main_ui.Ui_MainWindow):
         self.savedata_timer.start(1000*60*1)
 
         # data aggregate timer
-        self.aggdata_timer = QTimer()
-        self.aggdata_timer.timeout.connect(self.data.aggregate_data)
-        self.aggdata_timer.start(10000)#int(1000 * self.data.agg_intervals[0].total_seconds() * 1.5))
-        self.data.aggregate_data()
+        # self.aggdata_timer = QTimer()
+        # self.aggdata_timer.timeout.connect(self.data.aggregate_data)
+        # self.aggdata_timer.start(10000)#int(1000 * self.data.agg_intervals[0].total_seconds() * 1.5))
+        # self.data.aggregate_data()
 
         # set starting screen
         self.stackedWidget.setCurrentIndex(0)
