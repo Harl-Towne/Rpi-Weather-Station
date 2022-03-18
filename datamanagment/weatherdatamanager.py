591bfcab6e332bd4bda5f247aea120ac9c72599d
Harl Towne <harl.towne@connect.qut.edu.au>
2022-03-18 10:34:39 +1000

    Sync

--- datamanagment/weatherdatamanager.py
+++ datamanagment/weatherdatamanager.py
@@ -118,9 +118,7 @@ class WeatherData(datamanager.Data):
                     if column == "rain":
                         agg_datum[column] = [ranged_data[column].sum()]
                     elif column == "wind_direction":
-                        print(ranged_data.mode())
-                        print(ranged_data[column].mode(dropna=False))
-                        if pd.isnull(ranged_data[column]).all():
+                        if pd.isnull(ranged_data[column]).all(): # if all values are NaN then .mode() returns an empty array and causes an index out of range error
                             agg_datum[column] = np.NaN
                         else:
                             agg_datum[column] = [ranged_data[column].mode()[0]]
