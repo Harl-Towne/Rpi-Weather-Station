23a928c140303d82ca5c0ac79e1a7f51e566aff7
Harl Towne <harl.towne@connect.qut.edu.au>
2022-03-18 10:29:35 +1000

    Sync

--- datamanagment/weatherdatamanager.py
+++ datamanagment/weatherdatamanager.py
@@ -118,6 +118,7 @@ class WeatherData(datamanager.Data):
                     if column == "rain":
                         agg_datum[column] = [ranged_data[column].sum()]
                     elif column == "wind_direction":
+                        print(ranged_data.mode())
                         if pd.isnull(ranged_data[column]).all():
                             agg_datum[column] = np.NaN
                         else:
